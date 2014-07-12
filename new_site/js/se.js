6fff7e6a8a226b8d0951103f9e5185fd8df8f5e3
mike chin <mike.chin@concet3D.com>
2014-07-12 11:06:19 -0600

    add routing.

--- new_site/js/se.js
+++ new_site/js/se.js
@@ -1,1 +1,29 @@
-var se = angular.module('se', []);
+var se = angular.module('se', ['ngRoute']);
+
+se.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.
+		when('/', {
+			templateUrl: 'templates/home.html'
+		}).
+		when('/class', {
+			templateUrl: 'templates/class.html'
+		}).
+		when('/faq', {
+			templateUrl: 'templates/faq.html'
+		}).
+		when('/news', {
+			templateUrl: 'templates/news.html'
+		}).
+		when('/photos', {
+			templateUrl: 'templates/photos.html'
+		}).
+		when('/about', {
+			templateUrl: 'templates/about.html'
+		}).
+		when('/contact', {
+			templateUrl: 'templates/contact.html'
+		}).
+		otherwise({
+			redirectTo: '/'
+		})
+}]);
